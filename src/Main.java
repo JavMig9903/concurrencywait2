c026cdd595a195abdaf77aaaf542b36543264c8f
JavMig9903 <javiermiguel9903@gmail.com>
2020-10-29 12:09:38 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static final ArrayList<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+       Camarero camarero = new Camarero();
+    }
+
+
+}
